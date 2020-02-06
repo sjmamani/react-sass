179fbb4b1eb8ef709bba4de73ca844b0792c9177
Santiago Mamani <42153383+sjmamani@users.noreply.github.com>
2020-02-05 21:03:58 -0300

    Updates

---
+++ precache-manifest.91535f0209aa40c00001413a99bfa94f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe208c0e56478acf253087e962ff436",
+    "url": "/react-sass/index.html"
+  },
+  {
+    "revision": "bfbeda53c7557d58db64",
+    "url": "/react-sass/static/css/main.e6fe2efa.chunk.css"
+  },
+  {
+    "revision": "bba07c6a8f0fb5a87c52",
+    "url": "/react-sass/static/js/2.63437a84.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-sass/static/js/2.63437a84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bfbeda53c7557d58db64",
+    "url": "/react-sass/static/js/main.02c41ec0.chunk.js"
+  },
+  {
+    "revision": "43e014316918dc3c1a24",
+    "url": "/react-sass/static/js/runtime-main.1f8b0bb1.js"
+  },
+  {
+    "revision": "831c5581a9ef89f1a09da3a47bfc137f",
+    "url": "/react-sass/static/media/alexandros-nicolopoulos-Q_vvoClk0Pk-unsplash.831c5581.jpg"
+  }
+]);
